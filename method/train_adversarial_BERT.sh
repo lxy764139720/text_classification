b5ebd3523c4f7b240360c73cc6a79f74278bba8c
lxy764139720 <764139720@qq.com>
2021-05-11 00:55:51 +0800

    fix: 修复评估函数，修改训练脚本使用cpu

--- method/train_adversarial_BERT.sh
+++ method/train_adversarial_BERT.sh
@@ -2,10 +2,9 @@
 #SBATCH --job-name=train_adversarial_bert_1
 #SBATCH --output=test.out.%j
 #SBATCH --error=vasp.err.%j
-#SBATCH --partition=gpu4Q
+#SBATCH --partition=cpuQ
 #SBATCH --nodes=1
-#SBATCH	--cpus-per-task=4
-#SBATCH --gpus=1
+#SBATCH	--cpus-per-task=32
 #SBATCH --qos=gpuq
 
 source activate tf2.2
